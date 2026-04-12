1ca01b84dbc22ccf1eac4a03f2b6943487528fb0
AnushkaPatil542 <anushkapatil2112@gmail.com>
2026-04-12 08:19:50 -0700

    Server cors changes

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,10 @@ io.on("connection", (socket) => {
 });
 
 /* ================= MIDDLEWARE ================= */
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

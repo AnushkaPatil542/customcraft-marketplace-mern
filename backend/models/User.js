dd359163120ffb17fcf6249468953ef3a639757f
AnushkaPatil542 <anushkapatil2112@gmail.com>
2026-04-12 13:15:04 -0700

    remove app.options crash fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -44,16 +44,20 @@ const userSchema = new mongoose.Schema(
 );
 
 /* 🔐 HASH PASSWORD BEFORE SAVE */
-const bcrypt = require("bcryptjs");
-
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  try {
+    // Only hash if password changed
+    if (!this.isModified("password")) return next();
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 
-  next();
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
 /* ❌ REMOVE matchPassword (NOT NEEDED ANYMORE) */
 
 module.exports = mongoose.model("User", userSchema);

0e41d10ba2d873dca8995ebb03e38feadf5ad9f8
AnushkaPatil542 <anushkapatil2112@gmail.com>
2026-04-13 00:19:04 -0700

    check mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("🔥 MongoDB Connected Successfully");
+    console.log("DB NAME:", conn.connection.name);
+
   } catch (error) {
-    console.error(error.message);
+    console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
